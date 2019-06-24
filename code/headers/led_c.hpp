6c5bca7d58447925c7607d6ec33713bf18ff4b7c
streefje1996 <stefan@hammetjes.nl>
2019-06-24 15:25:05 +0200

    added constructors for led_c

--- code/headers/led_c.hpp
+++ code/headers/led_c.hpp
@@ -10,6 +10,13 @@ private:
     rgb_s color;
 
 public:
+    led_c() {};
+
+	template <typename T>
+	led_c(const T &new_color) {
+        set_color(new_color);
+	}
+
     void set_color(const rgb_s &rgb);
     void set_color(const hsv_s &hsv);
     void set_brightness(const uint8_t &percentage);
