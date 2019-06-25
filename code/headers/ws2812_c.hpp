795710fcfc3240e949f961c4e111d6107b89640c
streefje1996 <stefan@hammetjes.nl>
2019-06-25 02:41:59 +0200

    fixed huge issues, still a timing issue in ws2812_c.hpp send function

--- code/headers/ws2812_c.hpp
+++ code/headers/ws2812_c.hpp
@@ -13,11 +13,53 @@ namespace r2d2::led_strip {
         const uint16_t time_1_low_ns  = 600;
         const uint16_t time_reset_us  = 50;
 
+		uint8_t grb[3];
+
         hwlib::pin_out &data_pin;
-        void send(const rgb_s &rgb) override;
+        
 
     public:
         ws2812(hwlib::pin_out &data_out) 
 			: data_pin(data_out){};
+
+		void send() override {
+            data_pin.write(false);
+            data_pin.flush();
+            hwlib::wait_us(70);
+            uint16_t time;
+            uint16_t time2;
+            for (unsigned int current_led = 0; current_led < N; current_led++) {
+                //hwlib::cout << "     ";
+                rgb_s color = (*this)[current_led].get_color();
+                grb[0] = color.green;
+                grb[1] = color.red;
+                grb[2] = color.blue;
+
+                for (auto sending_color : grb) {
+                    for (unsigned int i = 0; i < 8; i++) {
+                        if ((sending_color & 128) == 128) {
+                            //hwlib::cout << "1";
+                            data_pin.write(true);
+                            //data_pin.flush();
+                            time = hwlib::now_us();
+                            hwlib::wait_ns_busy(time_1_high_ns);
+                            time2 = hwlib::now_us();
+                            data_pin.write(false);
+                            //data_pin.flush();
+                            hwlib::wait_ns_busy(time_1_low_ns);
+                        } else {
+                            //hwlib::cout << "0";
+                            data_pin.write(true);
+                            //data_pin.flush();
+                            hwlib::wait_ns_busy(time_0_high_ns);
+                            data_pin.write(false);
+                            //data_pin.flush();
+                            hwlib::wait_ns_busy(time_0_low_ns);
+                        }
+                        sending_color <<= 1;
+                    }
+                }
+            }
+		}
     };
 } // namespace r2d2::led_strip
--- code/main.cpp
+++ code/main.cpp
@@ -1,11 +1,25 @@
+#include "headers/ws2812_c.hpp"
 #include "hwlib.hpp"
 
 int main(void) {
-  // kill the watchdog
+    // kill the watchdog
     WDT->WDT_MR = WDT_MR_WDDIS;
     hwlib::wait_ms(1000);
-    for (;;){
-        hwlib::cout << "this works via arduino";
-        hwlib::wait_ms(1000);
+
+    auto weg = hwlib::target::pin_out(hwlib::target::pins::d8);
+
+    r2d2::led_strip::ws2812<2> ledjes(weg);
+
+    r2d2::led_strip::rgb_s kleur = {0, 0, 0};
+    r2d2::led_strip::rgb_s kleur2 = {0, 0, 20};
+
+    // ledjes.set_color(kleur);
+    ledjes[0].set_color(kleur2);
+    ledjes[1].set_color(kleur);
+    
+    ledjes.send();
+
+    for (;;) {
+        //ledjes.send();
     }
 }
