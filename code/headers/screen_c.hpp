646c6a492dbd6b86ac87aa10eb4631ce6c342d9b
streefje1996 <stefan@hammetjes.nl>
2019-07-01 22:09:43 +0200

    added screen

---
+++ code/headers/screen_c.hpp
@@ -0,0 +1,31 @@
+#pragma once
+
+#include <stdint.h>
+#include "led_strip_c.hpp"
+
+namespace r2d2::led_strip {
+	template <unsigned int N>
+    class screen_c {
+    private:
+        uint16_t width, height;
+        led_strip_c<N> &leds;
+    public:
+        screen_c(const uint16_t &width, const uint16_t &height,
+                 led_strip_c<N> &leds) 
+			: width(width), height(height), leds(leds) {}
+
+		void draw(const uint16_t &x, const uint16_t &y, const rgb_s &color = {10,10,10}) {
+            uint16_t index = (y % 2 == 1) ? ((width - x) - 1) : (x);
+            index += y * height;
+            leds[index].set_color(color);		
+		}
+
+        void clear() {
+            leds.set_color(rgb_s{0, 0, 0});
+        }
+
+        void update() {
+            leds.send();
+        }
+    };
+} // namespace r2d2::led_strip
