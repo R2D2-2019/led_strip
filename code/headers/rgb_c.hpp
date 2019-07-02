004341a14bf60a3d26fe5dba1e50bf02d0532076
Jesper Obbes <Jesper.obbes@student.hu.nl>
2019-07-02 11:28:05 +0200

    created rgb_c adt out of rgb_s struct

---
+++ code/headers/rgb_c.hpp
@@ -0,0 +1,70 @@
+#pragma once
+
+#include <stdint.h>
+
+namespace r2d2::led_strip {
+    /**
+     *  \brief
+     * rgb adt
+     * \details
+     * this is an ADT that implements rgb values
+     * the appropiate operators & constructor are provided
+     *  
+     */
+    class rgb_c {
+    private:   
+        uint8_t red;
+        uint8_t green;
+        uint8_t blue;
+
+    public:
+        rgb_c(const uint8_t & red, const uint8_t & green, const uint8_t & blue):
+        red(red),
+        green(green),
+        blue(blue)
+        {}
+
+        /**
+         * \brief
+         * compare two rgb_c values
+         * \details
+         * This operator tests for equality. It returns true
+         * if and only if the values of both operands are equal.
+         *
+         * @return returns true if both operands are equal 
+         */    
+        bool operator==(const rgb_c &rhs) const;
+
+        /**
+         * \brief
+         * multiply a rgb_c by an integer
+         * \details
+         * This operator* multiplies a rational value by an integer value.
+         */
+        rgb_c operator*(const int rhs) const;
+
+        /**
+         * \brief
+         * Multiply a rgb_c by a rgb_c
+         * \details
+         * This operator* multiplies a rgb_c value by a rgb_c value.
+         */
+        rgb_c operator*(const rgb_c & rhs) const;
+
+        /**
+         * \brief
+         * add a rgb_c to another rgb_c
+         * \details
+         * This operator+= adds a rgb_c value to a rgb_c value
+         */
+        rgb_c & operator+=(const rgb_c & rhs);
+
+        /** 
+         * \brief
+         * multiply a rgb_c with an integer
+         * \details
+         * This operator *= multiplies a rgb_c with an integer.
+         */
+        rgb_c & operator*=(const int & rhs );
+    };
+}
---
+++ code/src/rgb_c.cpp
@@ -0,0 +1,34 @@
+#include "rgb_c.hpp"
+
+
+bool r2d2::led_strip::rgb_c::operator==(const rgb_c &rhs) const {
+    return  red     == rhs.red      &&
+            green   == rhs.green    &&
+            blue    == rhs.blue; 
+}
+
+r2d2::led_strip::rgb_c r2d2::led_strip::rgb_c::operator*(const int rhs) const {
+    return rgb_c(red * rhs, green * rhs, blue * rhs );
+
+}
+
+r2d2::led_strip::rgb_c r2d2::led_strip::rgb_c::operator*(const rgb_c & rhs) const {
+    return rgb_c(
+        red     * rhs.red,
+        green   * rhs.green,
+        blue    * rhs.blue
+        );
+}
+r2d2::led_strip::rgb_c & r2d2::led_strip::rgb_c::operator+=(const rgb_c & rhs) {
+    red = red + rhs.red;
+    green = green + rhs.green;
+    blue = blue + rhs.blue;
+    return *this;
+}
+
+r2d2::led_strip::rgb_c & r2d2::led_strip::rgb_c::operator*=(const int & rhs ) {
+    red = red * rhs;
+    green = green * rhs;
+    blue = blue * rhs;
+    return *this;
+}
